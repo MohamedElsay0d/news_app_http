9c022b90d59af6fff6cdddc063b8e04236447de2
MohamedElsay0d <me827794@gmail.com>
2025-02-12 04:39:09 +0200

    constants ao api

---
+++ lib/data/services/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static const String apiKey = '5b00d2f9297947119f24905928bb2e69';
+  static const String baseUrl = 'newsapi.org';
+  static const String sourceEndpoint = '/v2/top-headlines/sources';
+  static const String newsEndpoint = '/v2/top-headlines';
+}
